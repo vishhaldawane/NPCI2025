2b24b55f738552b12609a32217e0b17d4803c6f7
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2025-10-06 18:44:17 +0530

    Add files via upload

--- DivideTest.java
+++ DivideTest.java
@@ -7,23 +7,30 @@ public class DivideTest {
 		
 		calci.divide("10", "5");
 		
-		try
-		{
-			calci.divide("10", "abc");
-		}
-		catch(NumberFormatException e) {
-			System.out.println("Please supply numeric value : "+e.getMessage());
-		}
-		catch(ArithmeticException e) {
-			System.out.println("Cannot divide by zero...."+e.getMessage());
-		}
+//		try
+//		{
+			calci.divide("10", "0");
+//		}
+//		catch(NumberFormatException e) {
+//			System.out.println("Some problem1 : Please supply numeric value : "+e.getMessage());
+//		}
+//		catch(ArithmeticException e) {
+//			System.out.println("Some problem2 : Cannot divide by zero...."+e.getMessage());
+//		}
+//		catch(RuntimeException e) {
+//			System.out.println("Some problem3 : "+e.getMessage());
+//		}
+//		catch(Exception e) {
+//			System.out.println("Some problem4 : "+e.getMessage());
+//		}
 		
-		try {
+		
+	//	try {
 			calci.divide("210", "3");//line 9
-		}
-		catch(ArithmeticException e) {
-			System.out.println("Problem : "+e.getMessage());
-		}
+	//	}
+//		catch(ArithmeticException e) {
+		//	System.out.println("Problem : "+e.getMessage());
+//		}
 		calci.divide("50", "3");
 		calci.divide("90", "8");
 		
@@ -39,11 +46,17 @@ class Calculator
 		System.out.println("divide started....");
 		int x = Integer.parseInt(num);
 		int y = Integer.parseInt(deno);
+		
 		System.out.println("num  : "+x);
 		System.out.println("deno : "+y);
-		int div = x / y;//throw new ArithmeticException("/ by zero"); // line 25
-		System.out.println("div  : "+div);
-		System.out.println("divide finished...");
+		if(y !=0 ) {
+			int div = x / y;//throw new ArithmeticException("/ by zero"); // line 25
+			System.out.println("div  : "+div);
+			System.out.println("divide finished...");
+		}
+		else {
+			System.out.println("CANNOT DIVIDE BY ZERO (old way) ..... ");
+		}
 		System.out.println("-----------------------------");
 	}
 }
