05177ebfa3edbad32b886482cef8cacc3351b9c1
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2025-10-07 13:07:35 +0530

    Frame

--- ThreadTest.java
+++ ThreadTest.java
@@ -1,59 +1,84 @@
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
 public class ThreadTest {
 	public static void main(String[] args) {
 		//3.
-		Car car1 = new Car("Skoda");
-		Bike bike = new Bike("\tPulsar");
-		Train train = new Train("\t\tVande Bharat");
+		Car car1 = new Car("Skoda",100,100,150,250);
+		Bike bike = new Bike("Pulsar",400,100,150,250);
+		Train train = new Train("Rajdhani",800,100,150,250);
 		
 		//4. 
 		Thread t1 = new Thread(car1);
 		Thread t2 = new Thread(bike);
 		Thread t3 = new Thread(train);
 		
-		t1.start(); 		t2.start();		t3.start();		
+		t1.start(); 		t2.start();		t3.start();		//5
 	}
 }
 class Vehicle { }
-
-class Car extends Vehicle implements Runnable//1
+class Car extends JFrame implements Runnable//1
 {
 	String model;
+	JTextField field = new JTextField(20);
 	
-	Car(String model) {
+	Car(String model,int x, int y, int h, int w) {
 		this.model = model;
+		setLocation(x,y);
+		setSize(w,h);
+		add(field);
+
+		setVisible(true);
 	}
 	public void run() { //2
-		for(int i=1;i<=300;i++) {
+		for(int i=1;i<=3000000;i++) {
 			System.out.println("Car is running..."+model+ " i "+i);
+			field.setText("Car is running..."+model+" i "+i);
+
 		}
 	}
 }
 
-class Bike extends Vehicle implements Runnable//1
+class Bike extends JFrame implements Runnable//1
 {
 	String model;
-	
-	Bike(String model) {
+	JTextField field = new JTextField(20);
+
+	Bike(String model,int x, int y, int h, int w) {
 		this.model = model;
+		setLocation(x,y);
+		setSize(w,h);
+		add(field);
+		setVisible(true);
 	}
 	public void run() { //2
-		for(int i=1;i<=30;i++) {
+		for(int i=1;i<=3000000;i++) {
 			System.out.println("Bike is running..."+model+ " i "+i);
+			field.setText("Bike is running..."+model+" i "+i);
+
 		}
 	}
 }
 
 
-class Train extends Vehicle implements Runnable//1
+class Train extends JFrame implements Runnable//1
 {
 	String model;
-	
-	Train(String model) {
+	JTextField field = new JTextField(20);
+
+	Train(String model,int x, int y, int h, int w) {
 		this.model = model;
+		setLocation(x,y);
+		setSize(w,h);
+		add(field);
+
+		setVisible(true);
 	}
 	public void run() { //2
-		for(int i=1;i<=1000;i++) {
+		for(int i=1;i<=3000000;i++) {
 			System.out.println("Train is running..."+model+ " i "+i);
+			field.setText("Train is running..."+model+" i "+i);
+			
 		}
 	}
 }
