eaffe1100db53171cff52308b729492ed61383d2
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2025-10-07 11:13:50 +0530

    SpeedLimitException

--- UserDefinedExceptionTest.java
+++ UserDefinedExceptionTest.java
@@ -4,24 +4,32 @@ public class UserDefinedExceptionTest {
 		try {
 			SedanCar sedanCar = new SedanCar("Skoda");
 			sedanCar.startTheCar();
+			sedanCar.longDrive();
 		}
 		
-		catch(RuntimeException e) {	System.out.println("Some Car problem : "+e.getMessage()); }
+		catch(RuntimeException e) {	
+			System.out.println("Some Car problem : "+e.getMessage());
+		}
 		catch(CarKeyNotFoundException carKeyEx) {
 			System.out.println("Car Problem : "+carKeyEx.getMessage());
 		}
 		System.out.println("End main");		
 	}
 }
-class Car {}
+class Car {
+	
+}
 class SedanCar extends Car {
 	boolean carKeyFound=false;	
 	
 	SedanCar(String model) throws CarKeyNotFoundException //mandatory for checked type
 	{
-		System.out.println("Car() ctor started...");	double keyFoundRandomValue = Math.random()%10;
-		if(keyFoundRandomValue > 0.80) {
-			carKeyFound = true; System.out.println("Car key found : "+carKeyFound); startTheCar();
+		System.out.println("Car() ctor started...");	
+		double keyFoundRandomValue = Math.random()%10;
+		
+		if(keyFoundRandomValue > 0.50) {
+			carKeyFound = true; 
+			System.out.println("Car key found : "+carKeyFound); startTheCar();
 		}
 		else { //System.out.println("Car key NOT found Exception....");
 			//throw new RuntimeException("Car key not found exception.....");
@@ -29,13 +37,34 @@ class SedanCar extends Car {
 		}
 		System.out.println("Car() ctor over...");
 	}
-	public void startTheCar() { System.out.println("Car started....");         }
+	public void startTheCar() { 
+		System.out.println("Car started....");       
+	}
+	public void longDrive() {
+		for(int i=1;i<=30;i++) {
+			System.out.println("Car long drive completed : "+i+" kms");
+			double speedLimit= Math.random()%10;
+			if(speedLimit > 0.95) {
+				throw new SpeedLimitException("OMG, Car exceeded the speed of 80 kmph");
+			}
+		
+		}
+		System.out.println("Long drive over....");
+	}
 }
 
 
 class CarKeyNotFoundException extends Exception
 {
 	CarKeyNotFoundException(String msg) {
+		super(msg);
+	}
+}
+
+
+class SpeedLimitException extends RuntimeException
+{
+	SpeedLimitException(String msg) {
 		super(msg);
 	}
 }
