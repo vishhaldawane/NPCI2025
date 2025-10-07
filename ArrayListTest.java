6e07b494e95d81f4d10d8c09a42cb11ed593587d
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2025-10-07 18:19:34 +0530

    Add files via upload

--- ArrayListTest.java
+++ ArrayListTest.java
@@ -1,13 +1,17 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.TreeSet;
 
 public class ArrayListTest {
 	public static void main(String[] args) {
-		PhoneLog log1 = new PhoneLog("Dialled",LocalDate.now(),"Rejected",0,"Jane");
-		PhoneLog log2 = new PhoneLog("Missed",LocalDate.now(),"Rejected",0,"Jack");
-		PhoneLog log3 = new PhoneLog("Received",LocalDate.now(),"Accepted",10,"Janet");
-		PhoneLog log4 = new PhoneLog("Missed",LocalDate.now(),"Rejected",0,"Julie");
-		PhoneLog log5 = new PhoneLog("Dialled",LocalDate.now(),"Accepted",5,"Julia");
+		PhoneLog log1 = new PhoneLog("Dialled",LocalDate.now(),"Rejected",10,"Jane");
+		PhoneLog log2 = new PhoneLog("Missed",LocalDate.now(),"Rejected",5,"Jack");
+		PhoneLog log3 = new PhoneLog("Received",LocalDate.now(),"Accepted",12,"Janet");
+		PhoneLog log4 = new PhoneLog("Missed",LocalDate.now(),"Rejected",3,"Julie");
+		PhoneLog log5 = new PhoneLog("Dialled",LocalDate.now(),"Accepted",15,"Julia");
 		
 /*		PhoneLog logArray [] = new PhoneLog[5]; //fixed size / cannot grow
 		logArray[0] = log1;
@@ -18,31 +22,64 @@ public class ArrayListTest {
 	//	logArray[5] = new PhoneLog("Dialled",LocalDate.now(),"Accepted",5,"Robert");
 		
 		//ctrl+shift+m <-- auto import
-		ArrayList<PhoneLog> logList = new ArrayList<PhoneLog>(); 
+	//	ArrayList<PhoneLog> logCollection = new ArrayList<PhoneLog>(); 
+		
+		//Glass
+	//	LinkedList<PhoneLog> logCollection = new LinkedList<PhoneLog>(); 
+
+	//	HashSet<PhoneLog> logCollection = new HashSet<PhoneLog>();
+		
+		TreeSet<PhoneLog> logCollection = new TreeSet<PhoneLog>();
+
 		System.out.println("Container is created...");
 		
+		System.out.println("Adding first object");
 		
-		logList.add(log1);
-		logList.add(log2);
-		logList.add(log3);
-		logList.add(log4);
-		logList.add(log5);
+		logCollection.add(log1); //add water 30ml
+		
+		System.out.println("Adding second object");
+		logCollection.add(log2); //add water 30ml
+		
+		System.out.println("Adding third object");
+		logCollection.add(log3); //add water 30ml
+		
+		System.out.println("Adding fourth object");
+		logCollection.add(log4); //add water 30ml
+		
+		System.out.println("Adding fifth object");
+		logCollection.add(log5); //add water 30ml
+		
 		System.out.println("Content is added to the Container...");
 		
-		for (PhoneLog phoneLog : logList) {
+		for (PhoneLog phoneLog : logCollection) {
 			System.out.println("Log is : "+phoneLog);
 		}
+		System.out.println("------------");
 		
+		//Straw
+		Iterator<PhoneLog> phoneLogIterator = logCollection.iterator();
+		while(phoneLogIterator.hasNext()) {
+			PhoneLog theLog = phoneLogIterator.next();
+			System.out.println("The log is : "+theLog);
+		}
 		
 	}
 }
-class PhoneLog
+class PhoneLog implements Comparable<PhoneLog>
 {
 	String logType; //missed,dialled, recieved
 	LocalDate date;
 	String  status;
 	int duration;
 	String name;
+	
+	@Override
+	public int compareTo(PhoneLog o) {
+		// TODO Auto-generated method stub
+		System.out.println("Comparing "+duration + " with "+o.duration);
+		return Integer.compare(duration, o.duration);
+	}
+	
 	public PhoneLog(String logType, LocalDate date, String status, int duration, String name) {
 		super();
 		this.logType = logType;
@@ -56,6 +93,7 @@ class PhoneLog
 		return "PhoneLog [logType=" + logType + ", status=" + status + ", duration=" + duration + ", name=" + name
 				+ "]";
 	}
+	
 	
 	
 }
